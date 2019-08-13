9ab1dfa25859d27cb652d321b82b5cc829e39b09
Zaranza <48480485+Zaranza@users.noreply.github.com>
2019-08-13 13:52:28 +0100

    Updates

---
+++ precache-manifest.39329665c20f096b0234ae45304c90f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29fdb1455d606469b0473d71fa77006f",
+    "url": "/todoz_reactjs/index.html"
+  },
+  {
+    "revision": "2a1e5b53bcdc9fdcdb21",
+    "url": "/todoz_reactjs/static/css/main.906cd648.chunk.css"
+  },
+  {
+    "revision": "21c178a277b6f64868c8",
+    "url": "/todoz_reactjs/static/js/2.ce5e4dec.chunk.js"
+  },
+  {
+    "revision": "2a1e5b53bcdc9fdcdb21",
+    "url": "/todoz_reactjs/static/js/main.ea2cc3aa.chunk.js"
+  },
+  {
+    "revision": "6861d90f156b9d1932c6",
+    "url": "/todoz_reactjs/static/js/runtime~main.a70ea9a1.js"
+  }
+]);
